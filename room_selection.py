679a07ad555467450801d7a31710cda054b430a5
codekeyur <keyurpatel2868@gmail.com>
2021-09-19 22:23:39 -0400

    python practice

---
+++ room_selection.py
@@ -0,0 +1,11 @@
+selected_option = str(input("Please select one options out of these four A B C D:"))
+if selected_option == "A":
+    print("Please go to the room No. 1")
+elif selected_option == "B":
+    print("Please go to the Room No. 2")
+elif selected_option == "C":
+    print("Please go to the Room no. 3")
+elif selected_option == "D":
+    print("Please go to the Room no. 4")
+else:
+    print("Please select a valid option.")
