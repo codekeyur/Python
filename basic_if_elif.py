2ee7c12a042e37032bd6d9365aabb27db311eaae
codekeyur <keyurpatel2868@gmail.com>
2021-09-19 22:22:04 -0400

    Basic if...elif operation

---
+++ basic_if_elif.py
@@ -0,0 +1,7 @@
+light = input("Is the Green Signal on or off ?:")
+if light == "on":
+    print("You can Proceed further.")
+elif light == "off":
+    print("You have to stop abnd eait until lights turns to Green.")
+else:
+    print("Traffic Signal having issue called someone.")
