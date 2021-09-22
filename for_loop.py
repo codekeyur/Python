16cb82a719c09aee9c7f79099ce3c03ed0587d77
codekeyur <keyurpatel2868@gmail.com>
2021-09-21 21:51:25 -0400

    for loop in Python.

---
+++ for_loop.py
@@ -0,0 +1,9 @@
+# # # #
+#Using for loop inside the function
+# # # #
+def std (name, clas, marks):
+ print("Name:" ,name)
+ print("Class:", clas)
+for x in marks:
+ print(x + marks)
+std (name="Kedi", clas = 10 , marks = 100)
